685e163a089a19c5233ddde397b023dc82acafbc
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-11-15 19:52:34 +0100

    Recuperar datos de preferencias

--- scripts/preferences.js
+++ scripts/preferences.js
@@ -5,3 +5,8 @@ function savePreferences() {
   localStorage.setItem("time-limit", timeLimit);
   $("#router-outlet").load("html/play.html");
 }
+
+function updatePreferencesView() {
+  $("#cards-number").val(localStorage.getItem("cards-number"));
+  $("#time-limit").val(localStorage.getItem("time-limit"));
+}
