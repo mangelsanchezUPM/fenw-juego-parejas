453f81dad2f19ebaf2c7a69e94fdda519cdf326c
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-11-15 21:07:51 +0100

    Records cargados

--- scripts/records.js
+++ scripts/records.js
@@ -1,12 +1,31 @@
-function savePreferences() {
-  let cardsNumber = parseInt($("#cards-number").val());
-  localStorage.setItem("cards-number", cardsNumber);
-  let timeLimit = parseInt($("#time-limit").val());
-  localStorage.setItem("time-limit", timeLimit);
-  $("#router-outlet").load("html/play.html");
+var http_request;
+
+function getRecords() {
+  const url = "http://fenw.etsisi.upm.es:10000/records";
+  http_request = new XMLHttpRequest();
+  http_request.open("GET", url, true);
+  http_request.onload = printRecords;
+  http_request.send();
 }
 
-function updatePreferencesView() {
-  $("#cards-number").val(localStorage.getItem("cards-number"));
-  $("#time-limit").val(localStorage.getItem("time-limit"));
+function printRecords() {
+  if (http_request.status == 200) {
+    let records = JSON.parse(http_request.response);
+    let content = records.map((record) => {
+      let date = new Date();
+      date.setTime(record.recordDate);
+      return `
+        <div class="container-fluid">
+          Usuario: ${record.username}
+          Puntuación: ${record.punctuation}
+          Número cartas: ${record.cards}
+          Fecha: ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}
+        </div><hr>`;
+    });
+    $("#records-body").html(content);
+  } else {
+    console.error("Error al recibir la respuesta.");
+    alert("Error al recibir la respuesta");
+    $("#router-outlet").load("html/home.html");
+  }
 }
